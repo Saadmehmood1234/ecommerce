96f8708c35fe59368fb0e65f8af05c42daa6f66f
Saadmehmood1234 <158064428+Saadmehmood1234@users.noreply.github.com>
2025-03-22 21:29:12 +0530

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div  className='text-red-600'>Home</div>
+    <div className='text-white text-2xl font-bold flex items-center justify-center h-screen bg-gradient-to-tr from-[#0E091C] via-[#1F133D] to-[#0B1027]'>
+      Home
+    </div>
   )
 }
 
