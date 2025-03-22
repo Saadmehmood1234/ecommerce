18a4dd7191de2dbbbf016038e228af25e6e759d1
kamlesh-Sahani <alex20150001969@gmail.com>
2025-03-22 23:38:43 +0530

    navbar changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,59 +1,58 @@
 "use client";
 import { Home, Search, ShoppingCart, User, Menu } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 const Navbar = () => {
-  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(true);
+
+  useEffect(() => {
+    // Function to check screen width
+    const handleResize = () => {
+      if (window.innerWidth >= 768) {
+        setMobileMenuOpen(false);
+      }
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
     <nav className="p-4">
-      <div className="container flex items-center justify-between">
+      <div className=" flex items-center bg-[#1F133D] justify-between fixed   z-[110] px-4 py-2 drop-shadow-md w-[90vw] top-6 left-[50%] translate-[-50%]  rounded-2xl h-[60px] mt-5">
         <div className="text-2xl font-bold">
           <h1 className="text-[#C27AFF] text-3xl">
             Dark<span className="text-white">Sale</span>
           </h1>
         </div>
-        <div className="hidden md:flex space-x-6">
+        <div className="flex gap-1 justify-center items-center">
           <form action="" className="flex items-center justify-center relative">
             <Search className="absolute left-2 text-[#6d6d6dc4]" size={20} />
             <input
               type="text"
               placeholder="Tinder, Amazon Prime, etc.."
-              className="h-[40px] outline-none border-none bg-[#1F133D] pl-8 rounded text-white w-[350px]"
+              className="h-[40px] outline-none border-none bg-[#310557] pl-8 rounded text-white w-[350px]"
             />
           </form>
           <Link href="/auth/register">
-            <button className="text-white bg-[#1F133D] w-[120px] h-[39px] font-bold rounded  cursor-pointer hover:bg-[#C27AFF] ">
+            <button className="text-white bg-[#310557] w-[120px] h-[39px] font-bold rounded  cursor-pointer hover:bg-[#1F133D] ">
               Sign up
             </button>
           </Link>
         </div>
-        <button
-          className="md:hidden"
-          onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
-        >
-          <Menu className="text-[#482307]" />
-        </button>
       </div>
+
       <div
-        className={`md:hidden fixed bottom-0 left-0 right-0 bg-[#FFF7E2] text-[#482307] p-4 shadow-lg ${
+        className={`md:hidden fixed bottom-0 left-0 right-0 bg-[#1F133D] text-white z-[110] p-4 shadow-lg ${
           isMobileMenuOpen ? "block" : "hidden"
         }`}
       >
         <div className="flex justify-around items-center">
           <Link href="#" className="flex flex-col items-center">
-            <Home className="text-[#A1724E]" />
+            <Home className="text-[#503277]" />
             <span>Home</span>
           </Link>
           <Link href="#" className="flex flex-col items-center">
-            <Search className="text-[#A1724E]" />
-            <span>Search</span>
-          </Link>
-          <Link href="#" className="flex flex-col items-center">
-            <ShoppingCart className="text-[#A1724E]" />
-            <span>Cart</span>
-          </Link>
-          <Link href="#" className="flex flex-col items-center">
-            <User className="text-[#A1724E]" />
+            <User className="text-[#503277]" />
             <span>Account</span>
           </Link>
         </div>
