93d99efa94dbf20cb5340e5f36cdb431f68d0b65
Saadmehmood1234 <158064428+Saadmehmood1234@users.noreply.github.com>
2025-03-23 22:25:26 +0530

    added

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -38,6 +38,7 @@ const Category = () => {
               }}
               className="flex flex-col items-center gap-3 cursor-pointer transform transition duration-300 hover:scale-105"
             >
+              
               <img
                 src={category.img}
                 alt={category.name}
